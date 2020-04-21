66ebd4efa95f1e3d3dbd050c4f5f09f7e3e5932d
didinele <didinele.dev@gmail.com>
2020-04-21 13:45:13 +0300

    feat: docker support (#3)
    * feat: docker files
    
    * chore: get it to build

---
+++ packages/bot/src/tasks/SetStatus.ts
@@ -0,0 +1,34 @@
+import { Task } from '@reika/common';
+import ReikaClient from '../client/ReikaClient';
+import { ActivityType } from 'discord.js';
+import { TOPICS } from '../util/Constants';
+
+export default class SetStatusTask extends Task {
+  public statuses = [
+    'Watching over y\'all',
+    'Listening to JoJo\'s OSTs'
+  ];
+
+  public constructor() {
+    super('setStatus', {
+      period: 3600000,
+      category: 'client',
+      once: false
+    });
+  }
+
+  public exec(client: ReikaClient) {
+    const status = this.statuses[Math.floor(Math.random() * this.statuses.length)].split(' ');
+    const type = status.splice(0, 1)[0].toUpperCase();
+    if (type === 'Listening') status.splice(0, 1);
+    const message = status.join(' ');
+
+    return client.user?.setPresence({
+      activity: {
+        type: type as ActivityType,
+        name: message
+      }
+    })
+    .catch(e => client.logger.error(e.stack ?? e.message, { topic: TOPICS.BOT.ERROR }));
+  }
+}
