6cbadc7eb9ac763228da3b236e315fa90db7dd3b
didinele <didinele.dev@gmail.com>
2020-04-29 23:29:28 +0300

    feat: new logger (#4)

--- packages/common/lib/util/logger.ts
+++ packages/common/lib/util/logger.ts
@@ -1,37 +1,26 @@
 import { createLogger, format, transports } from 'winston';
-import * as DailyRotateFile from 'winston-daily-rotate-file';
-import { join } from 'path';
-import { existsSync, mkdirSync } from 'fs';
+import WSTransport from '@weeb-cafe/winston-ws';
 
-export default (name: string, path?: string) => {
-  path = path || join(process.cwd(), 'logs');
-
-  if (!existsSync(path)) mkdirSync(path);
-
-  return createLogger({
-    format: format.combine(
-      format.errors({ stack: true }),
-      format.label({ label: name.toUpperCase() }),
-      format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
-      format.printf(info => {
-        const { timestamp, label, level, message, topic, ...rest } = info;
-        return `[${timestamp}][${label}][${level.toUpperCase()}][${topic}]: ${message}${
-          Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}` : ''
-        }`;
-      })
-    ),
-    transports: [
-      new transports.Console({
-        format: format.colorize({ level: true }),
-        level: 'info'
-      }),
-      new DailyRotateFile({
-        format: format.combine(format.timestamp(), format.json()),
-        level: 'debug',
-        dirname: path,
-        filename: `${name}-%DATE%.log`,
-        maxFiles: '14d'
-      })
-    ]
-  });
-};
+export default async (name: string) => createLogger({
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.label({ label: name.toUpperCase() }),
+    format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
+    format.printf(info => {
+      const { timestamp, label, level, message, topic, ...rest } = info;
+      return `[${timestamp}][${label}][${level.toUpperCase()}][${topic}]: ${message}${
+        Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}` : ''
+      }`;
+    })
+  ),
+  transports: [
+    new transports.Console({
+      format: format.colorize({ level: true }),
+      level: 'info'
+    }),
+    await new WSTransport({
+      format: format.combine(format.timestamp(), format.json()),
+      level: 'debug'
+    }).init(process.env.LOGGER_HOST!, process.env.LOGGER_ID!, process.env.LOGGER_TOKEN!) as any
+  ]
+});
