f6599745f9646ca342819c1fa64addc0a08e6f5b
didinele <didinele.dev@gmail.com>
2020-05-12 10:30:30 +0300

    fix(Logger): only init transport in prod (#10)
    * fix(Logger): only init the WS transport if in prod

--- packages/common/lib/util/logger.ts
+++ packages/common/lib/util/logger.ts
@@ -1,26 +1,31 @@
 import { createLogger, format, transports } from 'winston';
 import WSTransport from '@weeb-cafe/winston-ws';
 
-export default async (name: string, host: string, id: string, token: string) => createLogger({
-  format: format.combine(
-    format.errors({ stack: true }),
-    format.label({ label: name.toUpperCase() }),
-    format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
-    format.printf(info => {
-      const { timestamp, label, level, message, topic, ...rest } = info;
-      return `[${timestamp}][${label}][${level.toUpperCase()}][${topic}]: ${message}${
-        Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}` : ''
-      }`;
-    })
-  ),
-  transports: [
-    new transports.Console({
-      format: format.colorize({ level: true }),
-      level: 'info'
-    }),
-    await new WSTransport({
+export default async (name: string, host: string, id: string, token: string) => {
+  const used = [new transports.Console({
+    format: format.colorize({ level: true }),
+    level: 'info'
+  })];
+
+  if (process.env.NODE_ENV === 'production') {
+    used.push(await new WSTransport({
       format: format.combine(format.timestamp(), format.json()),
       level: 'debug'
-    }).init(host, id, token) as any
-  ]
-});
+    }).init(host, id, token) as any);
+  }
+
+  return createLogger({
+    format: format.combine(
+      format.errors({ stack: true }),
+      format.label({ label: name.toUpperCase() }),
+      format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
+      format.printf(info => {
+        const { timestamp, label, level, message, topic, ...rest } = info;
+        return `[${timestamp}][${label}][${level.toUpperCase()}][${topic}]: ${message}${
+          Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}` : ''
+        }`;
+      })
+    ),
+    transports: used
+  });
+};
